9eaffdf4a8a1d7c0fa02dd8037f9b71cc9d35e9e
cflb <cleyton.flb@gmail.com>
2021-08-26 19:57:58 -0300

    removendo projeto antigo

--- iamdev/webpages/controllers.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, Blueprint
-from iamdev import app
-
-home_blueprint = Blueprint('home', __name__)
-@home_blueprint.get('/')
-def home():
-    return render_template('webpages/home.html')
-
-
